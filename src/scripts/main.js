7ae2193e124f376be25bd56a4b1c0955d6a2085f
Ron San Jose <ronsj1@gmail.com>
2017-05-27 22:25:51 -0700

    Fetch and display quote text

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
 ((window, document) => {
 
   const RQM = (() => {
-    
-    const API_URL = 'https://quotesondesign.com/wp-json/posts';
+
+    const API_URL = '//quotesondesign.com/wp-json/posts';
 
     const _getQuote = () => {
       fetch(`${API_URL}?filter[orderby]=rand&filter[posts_per_page]=1`)
@@ -10,21 +10,41 @@
           if (res.ok) {
             return res.json();
           } else {
-            throw new Error("Response not OK");
+            throw new Error('Response not OK');
           }
         })
         .then(resData => {
-          console.log(resData);
+          _displayQuote(resData[0].content);
         })
         .catch(err => {
           console.log(err);
         });
     }
 
-    _getQuote();
+    const _displayQuote = (content) => {
+      const el = document.querySelector('#quote--text');
+      el.innerHTML = _stripHtmlTags(content);
+    }
+
+    const _stripHtmlTags = str => str.replace(/<[^>]*>/g, '');
+
+    return {
+      init() {
+        _getQuote();
+        const quoteBtn = document.querySelector('#quote--btn')
+        quoteBtn.addEventListener('click', (e) => {
+          e.preventDefault();
+          _getQuote();
+        });
+      }
+    }
 
   })();
 
   window.RQM = RQM || {};
+
+  document.addEventListener('DOMContentLoaded', () => {
+    RQM.init();
+  });
 
 })(window, document);
